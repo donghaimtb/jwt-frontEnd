c9ed4b0d787b91013c91298e008aebf5acf18379
donghaimtb <donghaimtb@gmail.com>
2024-05-18 08:05:37 +0700

    video deo9.3.1

---
+++ src/components/Login/Login.js
@@ -0,0 +1,34 @@
+import "./Login.scss";
+const Login = (props) => {
+  return (
+    <div className="login-container mt-3">
+      <div className="container">
+        <div className="row">
+          <div className="content-left col-7 ">
+            <div className="brand"> Hoi dan IT</div>
+            <div className="detail">Learn everything</div>
+          </div>
+          <div className="content-right col-5 d-flex flex-column gap-3 py-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Email address or your phone number"
+            />
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Password"
+            />
+            <button className="btn btn-primary">Login</button>
+            <span className="text-center">Forgot your password</span>
+            <hr />
+            <div className="text-center">
+              <button className="btn btn-success">Create New Acount</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Login;
